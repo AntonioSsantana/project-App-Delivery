508714b57641b15061b9f0f71dff0f2c039280a3
Juan Pablo <juammgsvo@hotmail.com>
2023-04-06 20:42:52 -0300

    pagina de pedidos funcional

--- front-end/src/Components/CustomerOrder.jsx
+++ front-end/src/Components/CustomerOrder.jsx
@@ -28,13 +28,13 @@ export default function CustomerOrder({
       <p
         data-testid={ `customer_orders__element-order-date-${id}` }
       >
-        {data}
+        {data.slice(0, '10')}
 
       </p>
       <p
         data-testid={ `customer_orders__element-card-price-${id}` }
       >
-        {subtotal}
+        {`R$ ${subtotal}`}
 
       </p>
     </button>
@@ -45,5 +45,5 @@ CustomerOrder.propTypes = {
   id: PropTypes.number.isRequired,
   status: PropTypes.string.isRequired,
   data: PropTypes.string.isRequired,
-  subtotal: PropTypes.number.isRequired,
+  subtotal: PropTypes.string.isRequired,
 };
--- front-end/src/Pages/Orders.js
+++ front-end/src/Pages/Orders.js
@@ -10,13 +10,12 @@ export default function Order() {
   const [isLoading, setIsLoading] = useState(true);
 
   const history = useHistory();
-  const ENDPOINT = 'customer/orders';
 
   useEffect(() => {
     const orders = async () => {
       const { id } = JSON.parse(localStorage.getItem('user'));
       console.log(`localStorage id:${id}`);
-      const response = await apiPostGeneric(ENDPOINT, id);
+      const response = await apiPostGeneric('customer/orders', { id });
       console.log(response);
       setOrdersList(response);
       setIsLoading(false);
@@ -40,7 +39,7 @@ export default function Order() {
     <div>
       <NavBar nome={ userName } />
       <p />
-      {/* {
+      {
         isLoading
           ? <h1>is loading..</h1>
           : ordersList.map(({ id, status, saleDate, totalPrice }) => (
@@ -52,32 +51,7 @@ export default function Order() {
               subtotal={ totalPrice }
             />
           ))
-      } */}
-
-      {/*  <DetailBar /> */}
-      {/* <p />
-      {isLoading
-        ? <h1>is loading..</h1>
-        : productDetail.map((product, index) => (
-          <DetailCard
-            key={ index }
-            index={ index + 1 }
-            nome={ product.product.name }
-            unitValue={ product.product.price.toString().replace('.', ',') }
-            quantity={ product.quantity }
-            subtotal={
-              (product.product.price * product.quantity)
-                .toString().replace('.', ',')
-            }
-          />
-        ))}
-      <p /> */}
-      {/* <div>
-        Total: R$
-        <span data-testid="customer_order_details__element-order-total-price">
-          {totalValueDetails.toString().replace('.', ',')}
-        </span>
-      </div> */}
+      }
     </div>
   );
 }
